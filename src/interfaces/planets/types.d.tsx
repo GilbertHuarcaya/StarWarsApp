21891527aa6e8d1dda995e174860aeb890b96620
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2022-05-30 22:40:45 -0500

    Add interfaces for each Api value

---
+++ src/interfaces/planets/types.d.tsx
@@ -0,0 +1,16 @@
+export interface Planet {
+  name: string,
+  rotation_period: 24,
+  orbital_period: 4818,
+  diameter: 10200,
+  climate: string,
+  gravity: string,
+  terrain: string,
+  surface_water: 8,
+  population: 1000,
+  residents: [],
+  films: string[],
+  created: string,
+  edited: string,
+  url: string
+}
---
+++ src/interfaces/starships/types.d.tsx
@@ -0,0 +1,20 @@
+export interface Starship {
+  name: string,
+  model: string,
+  manufacturer: string,
+  cost_in_credits: 1000000000000,
+  length: 120000,
+  max_atmosphering_speed: string,
+  crew: string,
+  passengers: string,
+  cargo_capacity: 1000000000000,
+  consumables: string,
+  hyperdrive_rating: 4.0,
+  MGLT: 10,
+  starship_class: string,
+  pilots: [],
+  films: string[],
+  created: string,
+  edited: string,
+  url: string
+}
